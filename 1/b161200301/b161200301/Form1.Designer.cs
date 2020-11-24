106c833fc7bf0077d5eab9aeb9e0928fdb923da5
kubrakismet <58142308+kubrakismet@users.noreply.github.com>
2020-11-24 22:11:14 +0300

    Add files via upload

---
+++ 1/b161200301/b161200301/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace b161200301
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(120, 331);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(143, 119);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyadı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Telefon no :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(87, 287);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 32);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "KAYDET";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(226, 287);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 32);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "KAYITLARI LİSTELE";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(119, 29);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "mavi";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(221, 29);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 17);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "yeşil";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton10);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(226, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 53);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Renk seçimi";
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(15, 27);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(53, 17);
+            this.radioButton10.TabIndex = 24;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "kırmızı";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(15, 39);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(53, 17);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "kırmızı";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(119, 37);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(47, 17);
+            this.radioButton5.TabIndex = 17;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "mavi";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(221, 38);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(45, 17);
+            this.radioButton6.TabIndex = 18;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "yeşil";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(15, 45);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(53, 17);
+            this.radioButton7.TabIndex = 19;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "kırmızı";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(119, 45);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(47, 17);
+            this.radioButton8.TabIndex = 20;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "mavi";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(221, 45);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(45, 17);
+            this.radioButton9.TabIndex = 21;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "yeşil";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Location = new System.Drawing.Point(226, 111);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 65);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Renk seçimi";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton7);
+            this.groupBox3.Controls.Add(this.radioButton8);
+            this.groupBox3.Controls.Add(this.radioButton9);
+            this.groupBox3.Location = new System.Drawing.Point(226, 192);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(331, 71);
+            this.groupBox3.TabIndex = 23;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Renk seçimi";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(87, 232);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 479);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ 1/b161200301/b161200301/Form1.cs
@@ -0,0 +1,206 @@
+﻿/************************************************************
+**				   SAKARYA ÜNİVERİSTESİ                   
+**		BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ        
+**		 BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ          
+**		    NESNEYE DAYALI PROGRAMLAMA DERSİ                   
+**			    2017-2018 BAHAR DÖNEMİ                     
+**                                                       
+**		    ÖDEV NUMARASI.........: 1                
+**		   	ÖĞRENCİ ADI...........: KÜBRA KISMET     
+**			ÖĞRENCİ NUMARASI......: B161200301       
+**                                                       
+************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;//dosya islemleri icin kütüphane
+namespace b161200301
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) { } 
+        private void richTextBox1_TextChanged(object sender, EventArgs e) { }
+        private void button4_Click(object sender, EventArgs e)
+        {
+           FileStream dosyayaz = new FileStream("veriler.txt", FileMode.Append);
+           StreamWriter dosya = new StreamWriter(dosyayaz);    
+            string ad, soyad,tel;
+            ad = textBox1.Text;
+            soyad = textBox2.Text;
+            tel =textBox4.Text;
+
+            if (radioButton10.Checked) dosya.Write('k');
+            else if (radioButton2.Checked) dosya.Write('m');
+            else if (radioButton3.Checked) dosya.Write('y');
+
+            if (radioButton4.Checked) dosya.Write('k');
+            else if (radioButton5.Checked) dosya.Write('m');
+            else if (radioButton6.Checked) dosya.Write('y');
+
+            if (radioButton7.Checked) dosya.Write('k');
+            else if (radioButton8.Checked) dosya.Write('m');
+            else if (radioButton9.Checked) dosya.Write('y');
+
+            dosya.WriteLine(" " + ad + " " + soyad + " " + tel);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox4.Clear();
+            dosya.Close();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FileStream dosyaoku = new FileStream("veriler.txt", FileMode.Open,FileAccess.Read);
+            StreamReader oku = new StreamReader(dosyaoku); 
+            string yazi = oku.ReadLine();
+            int font = 0;
+            while (yazi != null) 
+            {
+                string[] satirlar = yazi.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries); //dizi bölme secenekleri boşluk girilene kadar
+                for(int i=0;i<satirlar.Length;i++)
+                { 
+                string satir = satirlar[i];
+                StringOkumaYardimcisi yardimci = new StringOkumaYardimcisi(satir);
+                StringBuilder sb = new StringBuilder();
+
+                for (int j = 0; j < 3; j++)
+                    sb.Append(yardimci.KarakterOku());
+
+                RenkSaglayici renkSaglayici = new RenkSaglayici(sb.ToString());
+                bool satirBasi = true;
+                while (!yardimci.SonaErisildi)
+                {
+                    if (!satirBasi)
+                    {
+                        richTextBox1.AppendText(" ");
+                        richTextBox1.Select(0, 0);
+                    }
+                    else
+                        satirBasi = false;
+                    yardimci.BosluklariAtla();
+                    int baslangic = richTextBox1.Text.Length;
+                    string metin = yardimci.BoslugaKadarOku();
+                    int uzunluk = metin.Length;
+                    richTextBox1.AppendText(metin);
+                    richTextBox1.Select(baslangic, uzunluk);
+                    richTextBox1.SelectionColor = renkSaglayici.RenkVer();
+                }
+                if (i < satirlar.Length - 1)
+                    richTextBox1.AppendText(Environment.NewLine);
+                }
+                if (font % 2 == 0)
+                {
+                    richTextBox1.SelectionFont = new Font("Arial", 8, FontStyle.Bold);
+                    richTextBox1.AppendText(Environment.NewLine);
+                }
+                else
+                    richTextBox1.AppendText(Environment.NewLine);
+
+                font++;
+                yazi = oku.ReadLine();
+            }
+            oku.Close();
+        }
+        class RenkSaglayici
+        {
+            Color[] colors;
+            int sayac = 0;
+            int bolen = 1;
+
+            public RenkSaglayici(string renkKodu) : this(renkKoduCoz(renkKodu)) { }
+
+            private static Color[] renkKoduCoz(string renkKodu)
+            {
+                if (renkKodu.Length == 3)
+                {
+                    Color[] sonuc = new Color[3];
+                    int i = 0;
+                    foreach (char kod in renkKodu)
+                    {
+                        switch (kod)
+                        {
+                            case 'k': sonuc[i] = Color.Red;   break;
+                            case 'm': sonuc[i] = Color.Blue;  break;
+                            case 'y': sonuc[i] = Color.Green; break;
+                            default:  throw new Exception("Desteklenmeyen renk kodu");
+                        }
+                        i++;
+                    }  return sonuc;
+                }
+                else
+                    throw new Exception("Desteklenmeyen renk kodu");
+            }
+            public RenkSaglayici(Color[] colors)
+            {
+                if (colors == null)
+                    this.colors = colors = new Color[3] { Color.Red, Color.Blue, Color.Green };
+                else
+                    this.colors = colors;
+                this.bolen = colors.Length;
+            }
+            public Color RenkVer()
+            {
+                Color sonuc = colors[sayac];
+                sayac++;
+                sayac = sayac % bolen;
+                return sonuc;
+            }
+        }
+        class StringOkumaYardimcisi
+        {
+            private string metin;
+            public bool SonaErisildi { get; private set; }
+            public int Index { get; private set; }
+
+            public StringOkumaYardimcisi(string okunacakMetin)
+            {  this.metin = okunacakMetin;  }
+            public char Gozetle()//metnin index bakıyor sadece arttırım yok
+            { return metin[Index]; }
+
+            public char KarakterOku()
+            {
+                if (SonaErisildi)
+                    return default(char);
+                char sonuc = metin[Index];
+                Index++;
+                if (Index == metin.Length)//sona erişildi mi
+                    SonaErisildi = true;
+                return sonuc;
+            }
+            public string BoslugaKadarOku()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                while (!SonaErisildi)
+                {
+                    char c = Gozetle();
+                    if (char.IsWhiteSpace(c))//whitespace boşluk
+                        break;
+                    else
+                        sb.Append(KarakterOku());
+                }
+                return sb.ToString();
+            }
+            public void BosluklariAtla()
+            {
+                while (!SonaErisildi)
+                {
+                    char c = Gozetle();
+                    if (char.IsWhiteSpace(c))
+                        KarakterOku();
+                    else break;
+                }
+            }           
+        }
+
+    }
+}
